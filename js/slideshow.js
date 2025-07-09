bb49fcc1522ca01dc0578bed0b10124874b3b37a
Jose Adrian <kerpador@gmail.com>
2025-07-08 23:33:12 -0600

    send it

---
+++ js/slideshow.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function($){
+	
+	alert("Hola");
+	$("#slideshow img:gt(0)").hide();
+
+	setInterval(function() { 
+	  $('#slideshow  img:first')
+	    .fadeOut(1000)
+	    .next()
+	    .fadeIn(1000)
+	    .end()
+	    .appendTo('#slideshow');
+	},  3000);
+
+}
